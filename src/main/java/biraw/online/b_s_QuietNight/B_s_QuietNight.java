f2c7cb2d39070655a25c5d8aabaa790d8a990f5e
Bpetiii <87673385+Bpetiii@users.noreply.github.com>
2024-12-06 17:39:12 +0100

    pasted motd

--- src/main/java/biraw/online/b_s_QuietNight/B_s_QuietNight.java
+++ src/main/java/biraw/online/b_s_QuietNight/B_s_QuietNight.java
@@ -7,12 +7,16 @@ public final class B_s_QuietNight extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         Bukkit.getPluginManager().registerEvents(new MobSpawnListener(),this);
-    }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        // Print the motd
+        this.getLogger().info(" ");
+        this.getLogger().info("O=========================================================O");
+        this.getLogger().info("   The B's B's QuietNight plugin has loaded successfully");
+        this.getLogger().info("        This is B's QuietNight for Minecraft 1.18 +");
+        this.getLogger().info("                       Author: BiRaw");
+        this.getLogger().info("         Discord: https://discord.gg/XwFqu7uahX :>");
+        this.getLogger().info("O=========================================================O");
+        this.getLogger().info(" ");
     }
 }
