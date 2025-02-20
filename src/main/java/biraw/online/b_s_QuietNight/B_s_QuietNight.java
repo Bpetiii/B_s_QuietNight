a9daa528e86386a0110ea1bcc30ee7a235599e84
Bpetiii <biropeter2008@gmail.com>
2025-02-20 15:49:55 +0100

    v.2

--- src/main/java/biraw/online/b_s_QuietNight/B_s_QuietNight.java
+++ src/main/java/biraw/online/b_s_QuietNight/B_s_QuietNight.java
@@ -1,12 +1,57 @@
 package biraw.online.b_s_QuietNight;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
+import de.leonhard.storage.Config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public final class B_s_QuietNight extends JavaPlugin {
 
+    private final Config config = new Config("config", this.getDataFolder().getPath());
+
+    private static List<EntityType> darknessNeedingMobs = new ArrayList<>(Arrays.asList(
+            EntityType.SPIDER, EntityType.ZOMBIE, EntityType.CREEPER,
+            EntityType.ZOMBIE_VILLAGER, EntityType.WITCH, EntityType.ENDERMAN,
+            EntityType.CAVE_SPIDER, EntityType.HUSK, EntityType.SKELETON,
+            EntityType.STRAY, EntityType.BOGGED
+    ));
+
+
+    public static List<EntityType> getMobs() {
+        return darknessNeedingMobs;
+    }
+
+    private static List<String> getMobNames() {
+        List<String> names = new ArrayList<>();
+
+        darknessNeedingMobs.forEach(type->{
+            names.add(type.name());
+        });
+
+        return names;
+    }
+
     @Override
     public void onEnable() {
+
+        List<String> mob_names = config.getOrSetDefault("MOBS",getMobNames());
+
+        darknessNeedingMobs.clear();
+
+        mob_names.forEach(name->{
+            try{
+                darknessNeedingMobs.add(EntityType.valueOf(name.strip()));
+            }
+            catch (Exception ignored) {
+                this.getLogger().severe(name + " is an invalid mob name!");
+            }
+        });
+
         Bukkit.getPluginManager().registerEvents(new MobSpawnListener(),this);
 
         // Print the motd
--- src/main/java/biraw/online/b_s_QuietNight/MobSpawnListener.java
+++ src/main/java/biraw/online/b_s_QuietNight/MobSpawnListener.java
@@ -1,35 +1,30 @@
 package biraw.online.b_s_QuietNight;
 
-import org.bukkit.entity.Creature;
-import org.bukkit.entity.EntityType;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
-import java.util.List;
 
 public class MobSpawnListener implements Listener
 {
-    private static final List<EntityType> darknessNeedingMobs = List.of(
-            EntityType.SPIDER,EntityType.ZOMBIE,EntityType.CREEPER,
-            EntityType.ZOMBIE_VILLAGER,EntityType.WITCH,EntityType.ENDERMAN,
-            EntityType.CAVE_SPIDER,EntityType.HUSK,EntityType.SKELETON,
-            EntityType.STRAY
-            );
 
     @EventHandler
     private void MobSpawn(CreatureSpawnEvent event){
-        // If the spawning is because natural reasons
+        // If the spawn location is in the OVERWORLD,
+        if (event.getLocation().getWorld().getEnvironment() != World.Environment.NORMAL) return;
+
+        // If the spawning is because natural reasons,
         if (event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL) return;
 
-        // And if the mob cannot spawn in light
-        if (!darknessNeedingMobs.contains(event.getEntity().getType())) return;
+        // If the mob cannot spawn in light,
+        if (!B_s_QuietNight.getMobs().contains(event.getEntity().getType())) return;
 
         // And if the mob tries to spawn under the sky
         if (event.getLocation().getBlock().getLightFromSky() > 0)
         {
             event.setCancelled(true); // Cancel it
         }
-                                      // Else just let it spawn
+                                      // Else, just let it spawn
     }
 }
